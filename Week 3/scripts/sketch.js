f7b0d437ea9fb9057ab65ea45b0ece8eef56a74b
Richard Way <sixoften2@gmail.com>
2023-02-01 14:04:39 -0700

    Set up file structure (in-class example included)

---
+++ Week 3/scripts/sketch.js
@@ -0,0 +1,11 @@
+function preload() {
+    catImage = loadImage("https://s7d2.scene7.com/is/image/Caterpillar/CM20200803-7d970-4b60a");
+}
+
+function setup() {
+    createCanvas(600,500);
+}
+
+function draw() {
+    background(catImage);
+}
